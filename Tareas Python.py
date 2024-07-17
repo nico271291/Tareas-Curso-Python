badd47621db3b8f0262d42dde05ab113f9b7bcd5
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2024-07-17 17:20:45 -0300

    tareas-python

--- Tareas Python.py
+++ Tareas Python.py
@@ -1,3 +1,4 @@
+"""
 #_______________________________________________________________-
 #CLASE 16 de Julio, analizar el código y explicar que hace cada linea, luego reemplazarlo por otra condición
 #WHILE
@@ -14,7 +15,8 @@ while n<10: # Uso la sentencia WHILE para indicar que mientras la variable n sea
 numero_secreto = 6
 intentos = 0
 
-print("Bienvenido al juego de adivinar el número del 1 al 10.")
+print("Bienvenido al juego Adivina el numero.")
+print("El juego consiste en adivinar que numero del 1 al 10 estoy pensando.")
 print("Tienes 3 intentos para adivinar el número.")
 
 while intentos < 3:
@@ -31,8 +33,58 @@ while intentos < 3:
 
 if intentos == 3 and intento != numero_secreto:
     print("¡Lo siento! No lograste adivinar el número :(. El número correcto era:", numero_secreto)
+"""
+
 #  IF ELSE ELIF
 # Construir un algoritmo con lo viste en clase bajo el mismo diagrama de flujo de la imagen que está en la carpeta assets del repo
+boca = "Boca"
+racing = "Racing"
+riber = "Riber"
+river = "River"
+independiente = "Independiente"
+san_Lorenzo = "San lorenzo"
+print("Bienvenido")
+tu_equipo = input ("ingresa tu equipo con la primer letra en mayúscula:")
+if tu_equipo == boca:
+    print("Sos del club más ganador del mundo, el verdadero GRANDE.")
+elif tu_equipo == riber:
+    print("Te crées grande por una final ganda, pero sos un descendido 26/06/2011")
+elif tu_equipo == river:
+    print("Lo siento, no reconozco tu club. Quizás quiciste decir:", riber)
+elif tu_equipo == independiente:
+    print("Se creen grandes, pero descendieron tambien, al igual que Riber.")
+elif tu_equipo == san_Lorenzo:
+    print("Vos no sos grande, solo tenes una Libertadores. El que me programo dice que solo los propios hinchas se creen grandes.")
+elif tu_equipo == racing:
+    print("Sos considerado grande, pero un club que por 30 años no ganó nada, Im Sorry, pero no es grnade, ademas descendiste vos también.")
+else:
+    print("Es un buen club, por lo menos tienen mas huevos que Riber, San Lorenzo e Independiente, solo que les falta plata.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
